bc2d3da4a681b8f503a76892a213d109e666b649
chetankosmi <chetankosmi24@gmail.com>
2025-10-06 17:00:07 +0530

    save this to prime-count

--- Even_odd.py
+++ Even_odd.py
@@ -1,18 +1,22 @@
-def count_even_odd(numbers):
-    even_count = 0
-    odd_count = 0
+def is_prime(num):
+    if num <= 1:
+        return False
+    for i in range(2, int(num**0.5) + 1):
+        if num % i == 0:
+            return False
+    return True
+
+def count_primes(numbers):
+    prime_count = 0
 
     for n in numbers:
-        if n % 2 == 0:
-            even_count += 1
-        else:
-            odd_count += 1
+        if is_prime(n):
+            prime_count += 1
 
-    print("Even numbers:", even_count)
-    print("Odd numbers:", odd_count)
+    print("Prime numbers:", prime_count)
 
 
 # Example input
 numbers = [2, 3, 4, 5, 6, 7, 9, 11, 13, 14]
 
-count_even_odd(numbers)
+count_primes(numbers)
